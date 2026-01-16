55d9172e9265f418631b729675c3a30efb41a9f4
lochleaf <200536258@student.georgianc.on.ca>
2026-01-15 23:18:41 -0500

    final submit
    added reflection

--- assignments/labs/lab_one/car.php
+++ assignments/labs/lab_one/car.php
@@ -18,3 +18,9 @@ $car = new auto();
 //getting the variables from the class to output the strings
 echo "<p> {$car->brand} {$car->model} {$car->year} {$car->color} </p>";
 
+//reflection
+
+/*One of the main challenges I faced during this assignment was forgetting an important step, which was instantiating a new instance of the object.I also struggled with how to properly broadcast the message because I kept getting errors. After looking at documentation, I realized that I needed to use the -> operator, which was something I had never seen before. This made it challenging to understand how to access properties and methods in PHP. Another challenge was understanding how PHP is both similar to and different from C#. It reminded me of C# in terms of using classes and objects and concatenation, but it also works differently in its own way, which took some time to adjust to. Additionally, the PDO code used to connect to the database was taken from example shown in class. We were told to either leave it blank or try to figure it out ourselves, but I wanted to mention that I would have found this part challenging as well. What I found easier was creating the class, defining the variables properties, instantiating a new object, adding comments, and using include and required. I was a bit confused at first about how these worked, but I understood them shortly after reviewing the instructions multiple times. Also need more clearfication on pdo */
+
+//citation
+//https://www.php.net/manual/en/language.types.object.php
--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -3,3 +3,4 @@ require "header.php";
 echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
 require "footer.php"; 
 include "car.php";
+include "connect.php";
